8a09c46ea88717508d3361d43f704ba7139d09a9
Jacob Clayden <me@jacobcx.dev>
2026-02-24 11:35:22 +0000

    chore(android): update package dependencies for cross-platform support

--- Package.swift
+++ Package.swift
@@ -23,10 +23,10 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.6.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.6.0"),
+    .package(path: "../xctest-dynamic-overlay"),
     .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0"),
-    .package(url: "https://source.skip.tools/skip-fuse.git", from: "1.0.0"),
-    .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.0.0"),
+    .package(path: "../skip-fuse"),
+    .package(path: "../skip-fuse-ui"),
   ],
   targets: [
     .target(
